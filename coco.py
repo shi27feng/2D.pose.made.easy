363e5d5d57cab57e5dad7d2c15306a4f8ad821ab
Feng Shi <shi.feng@cs.ucla.edu>
2019-10-28 16:20:16 -0700

    updated file "coco.py"

--- coco.py
+++ coco.py
@@ -7,8 +7,9 @@ except ImportError:
     import numpy as np
 
 import cv2
+import torch
 import torch.utils.data as data
-from utils import prepare_annotations, _make_all_in_one_keypoints_map, _make_mask
+from utils import prepare_annotations, _make_all_in_one_keypoints_map, _make_mask, normalize_image, pad_image
 
 
 class CocoDataset(data.Dataset):
@@ -68,6 +69,30 @@ def evaluate(net,
     return
 
 
-def inference(net, img, scales):
-    
-    return
+def inference(net,
+              img,
+              scales,
+              base_height,
+              stride,
+              pad_value=(0, 0, 0),
+              mean_img=(128, 128, 128),
+              img_scale=1/256):
+    normalized_img = normalize_image(img, mean_img, img_scale)
+    height, width, _ = normalized_img.shape
+    scales_ratios = [scale * base_height / float(height) for scale in scales]
+    avg_ft_map = np.zeros((height, width, 3), dtype=np.float32)
+
+    for ratio in scales_ratios:
+        scaled_img = cv2.resize(normalized_img, (0, 0), fx=ratio, fy=ratio, interpolation=cv2.INTER_CUBIC)
+        min_dims = [base_height, max(scaled_img.shape[1], base_height)]
+        padded_img, pad = pad_image(scaled_img, stride, pad_value, min_dims)
+        tensor_img = torch.from_numpy(padded_img).permute(2, 0, 1).unsqueeze(0).float().cuda()
+        _ft_maps = net(tensor_img)
+        _ft_maps = np.transpose(_ft_maps.squeeze().cpu().data.numpy(), (1, 2, 0))
+        _ft_maps = cv2.resize(_ft_maps, (0, 0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
+        _ft_maps = _ft_maps[pad[0]: _ft_maps.shape[0] - pad[2],
+                            pad[1]: _ft_maps.shape[1] - pad[3]:, :]
+        _ft_maps = cv2.resize(_ft_maps, (width, height), interpolation=cv2.INTER_CUBIC)
+        avg_ft_map = avg_ft_map + _ft_maps / len(scales_ratios)
+
+    return avg_ft_map
