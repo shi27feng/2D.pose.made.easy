abf6356253e5326f1e31eebb13fa8b9b90858c66
yizhouzhao <yizhou_zhao@berkeley.edu>
2019-10-28 20:21:26 -0700

    Yizhou's code

--- loss.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-
-def l2loss(in_tenor, target, batch_size, mask=None):
-    diff = in_tenor - target if mask is None else torch.mul(in_tenor - target, mask)
-    loss = .5 * torch.pow(diff, 2) / batch_size
-    return torch.sum(loss)
